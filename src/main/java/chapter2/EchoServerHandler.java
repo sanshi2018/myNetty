5ac87d7687ed209cc4837a42bef5020d00e4804d
sanshi <191041656@qq.com>
2023-08-24 23:41:43 +0800

    init

---
+++ src/main/java/chapter2/EchoServerHandler.java
@@ -0,0 +1,39 @@
+package chapter2;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+// ChannelHandler.Sharable: 表示该 handler 可以被多个 channel 共享
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received: " + in.toString(CharsetUtil.UTF_8));
+        // 将接收到的消息写给发送者, 而不冲刷出站消息
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+        // 将未决消息冲刷到远程节点, 并且关闭该 Channel
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+            .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+        // 打印异常栈跟踪
+        cause.printStackTrace();
+        // 关闭该 Channel
+        ctx.close();
+    }
+}
